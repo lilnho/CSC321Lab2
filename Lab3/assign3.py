fcf7ebb056612dd3e6b16035b03394517bead4a1
Keila Mohan <keilamohan@hotmail.com>
2024-02-13 13:07:34 -0800

    diffie

--- Lab3/assign3.py
+++ Lab3/assign3.py
@@ -3,21 +3,47 @@ import random
 from Crypto.Hash import SHA256
 from Crypto.Cipher import AES
 
-def diffie(p, g):
-
+def diffie_ver1(p, g):
     # Alice
     a = random.randint(0, p)
     A = (g^a)%p
+    # send A
 
-
+    # Bob
     b = random.randint(0, p)
     B = (g^b)%p
+    # send B
 
+    # Alice
     sA = (B^a)%p
-    k = SHA256.new()
-    k.update(str(sA))
-    m = "Hi Bob!"
-    cA = 
+    k1 = SHA256.new()
+    k1.update(str(sA))
+    m0 = "Hi Bob!"
+    cipher = AES.new(k1, AES.MODE_CBC)
+    cA = cipher.encrypt(m0)
+
+    # send cA
+    print(cA)
+
+    # Bob
+    sB = (A^b)%p
+    k2 = SHA256.new()
+    k2.update(str(sB))
+    m1 = "Hi Alice"
+    cipher2 = AES.new(k2, AES.MODE_CBC)
+    cB = cipher2.encrypt(m1)
+
+    # send cB
+    print(cB)
+
+# def diffie_ver2(p, q):
+
+
+def main():
+    diffie_ver1(8, 10)
+
+if __name__ == "__main__":
+    main()
 
 
 
