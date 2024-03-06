fa96c057569346d3451988ef6b2a4b4c60a3e6e1
Keila Mohan <keilamohan@hotmail.com>
2024-03-06 12:58:48 -0800

    pc2

--- PC/passcrack.py
+++ PC/passcrack.py
@@ -1,13 +1,20 @@
+import nltk
+nltk.download('words')
 
+from nltk.corpus import words
 
 def main():
+
+    all_passes = words.words()
     strings = []
     with open('shadow_file.txt', 'r') as file:
         for line in file:
             strings.append(line)
 
     for string in strings:
-        # code
+        for word in all_passes:
+            if len(word) >= 6 and len(word) <= 10:
+                # code
 
 
 if __name__ == "__main__":
